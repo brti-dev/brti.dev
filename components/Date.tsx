8241231cdcb1741b18ce88b17a7c76bd30597210
dr-spaceman <mat.berti@gmail.com>
2021-05-21 00:51:52 +0800

    Fix Date component props

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,12 +1,13 @@
 import { parseISO, format } from 'date-fns'
-import propTypes from 'prop-types'
 
-function Date({ dateString }) {
-    const date = parseISO(dateString)
-    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+export type DateProps = {
+    date: string | Date
 }
-Date.propTypes = {
-    dateString: propTypes.string.isRequired,
+
+function Date({ date }: DateProps) {
+    const dateString = date.toString()
+    const dateParsed = parseISO(dateString)
+    return <time dateTime={dateString}>{format(dateParsed, 'LLLL d, yyyy')}</time>
 }
 
 export default Date
