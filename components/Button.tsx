c225fb3bd408cd04988e0db1c56b5955b366ec48
dr-spaceman <mat.berti@gmail.com>
2021-05-23 20:17:30 +0800

    Add Robato font + cleanup

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,45 +4,69 @@ import Link from 'next/link'
 export type ButtonProps = {
     type?: 'button' | 'reset' | 'submit'
     variant?: 'text' | 'contained' | 'outlined' | 'link' | 'icon' | 'close'
-    color?: 'default' | 'primary' | 'secondary' | 'red' | 'green' | 'dark' | 'light'
+    color?:
+        | 'default'
+        | 'primary'
+        | 'secondary'
+        | 'red'
+        | 'green'
+        | 'dark'
+        | 'light'
     size?: 'small' | 'medium' | 'large'
     className?: string
     to?: string
+    disabled?: boolean
+    loading?: boolean
     children: React.ReactNode
-} & React.HTMLAttributes<HTMLButtonElement> & React.HTMLAttributes<HTMLAnchorElement>
+} & React.ComponentPropsWithRef<'button'> &
+    React.ComponentPropsWithRef<'a'>
 
-function Button({
-    type = 'button',
-    variant = 'text',
-    color = 'default',
-    size = 'medium',
-    className,
-    to,
-    children,
-    ...rest
-}: ButtonProps) {
-    if (to) {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    (props, ref) => {
+        const {
+            type = 'button',
+            variant = 'text',
+            color = 'default',
+            size = 'medium',
+            className,
+            to,
+            disabled = false,
+            loading = false,
+            children,
+            ...rest
+        } = props
+
+        const classNames = [
+            'button',
+            `variant-${variant}`,
+            `color-${color}`,
+            `size-${size}`,
+            className && className,
+        ]
+
+        if (to) {
+            return (
+                <Link href={to}>
+                    <a className={classNames.join(' ')} {...rest}>
+                        {children}
+                    </a>
+                </Link>
+            )
+        }
+
         return (
-            <Link href={to}>
-                <a
-                    className={`button variant-${variant} color-${color} size-${size} ${className}`}
-                    {...rest}
-                >
-                    {children}
-                </a>
-            </Link>
+            <button
+                type={type}
+                className={classNames.join(' ')}
+                disabled={disabled || loading ? true : undefined}
+                data-loading={loading ? true : undefined}
+                ref={ref}
+                {...rest}
+            >
+                {children}
+            </button>
         )
     }
-
-    return (
-        <button
-            type={type}
-            className={`variant-${variant} color-${color} size-${size} ${className}`}
-            {...rest}
-        >
-            {children}
-        </button>
-    )
-}
+)
 
 export default Button
